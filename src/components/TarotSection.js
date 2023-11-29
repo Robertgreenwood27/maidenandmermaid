22fefc887d09824db727919e3850ca9993512ac8
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-11-29 05:25:52 -0700

    made some filler

--- src/components/TarotSection.js
+++ src/components/TarotSection.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 export default function TarotSection() {
-    return (
-        <p>TarotSection</p>
-    )
+  return (
+    <section className="my-20 text-center">
+      <h2 className="text-4xl font-bold mb-8">Explore the Tarot</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        {/* Tarot Card 1 */}
+        <div className="bg-black bg-opacity-50 rounded-lg p-6 shadow-lg border border-white text-white">
+          <h3 className="text-xl font-semibold mb-4">Card Title 1</h3>
+          <p className="text-gray-300">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit id odio nec laoreet.
+          </p>
+        </div>
+
+        {/* Tarot Card 2 */}
+        <div className="bg-black bg-opacity-50 rounded-lg p-6 shadow-lg border border-white text-white">
+          <h3 className="text-xl font-semibold mb-4">Card Title 2</h3>
+          <p className="text-gray-300">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit id odio nec laoreet.
+          </p>
+        </div>
+
+        {/* Tarot Card 3 */}
+        <div className="bg-black bg-opacity-50 rounded-lg p-6 shadow-lg border border-white text-white">
+          <h3 className="text-xl font-semibold mb-4">Card Title 3</h3>
+          <p className="text-gray-300">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit id odio nec laoreet.
+          </p>
+        </div>
+
+        {/* Add more tarot cards as needed */}
+      </div>
+    </section>
+  );
 }
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 export default function Testimonials() {
-    return (
-        <p>Testimonials</p>
-    )
+  // Replace the following placeholders with your actual testimonials
+  const testimonials = [
+    {
+      name: "John Doe",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit id odio nec laoreet.",
+    },
+    {
+      name: "Jane Smith",
+      text: "Suspendisse potenti. Praesent facilisis quam vel venenatis. Curabitur mattis turpis ut urna lacinia.",
+    },
+    {
+      name: "Alice Johnson",
+      text: "Vivamus at elit sit amet nunc egestas vestibulum ac vel turpis. Aenean nec viverra libero.",
+    },
+  ];
+
+  return (
+    <section className="my-20 text-center">
+      <h2 className="text-4xl font-bold mb-8 text-white">What Our Clients Say</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        {testimonials.map((testimonial, index) => (
+          <div key={index} className="bg-black bg-opacity-50 rounded-lg p-6 shadow-lg border border-white text-white">
+            <p className="mb-4">{testimonial.text}</p>
+            <p className="text-xl font-semibold">{testimonial.name}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
