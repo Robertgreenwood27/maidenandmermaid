15014d1f2f8295de497181714440f47910a83b8f
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-11-28 19:21:10 -0700

    first commit

---
+++ src/components/Background.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useRef } from 'react';
+
+const Background = () => {
+  const canvasRef = useRef(null);
+  const lightningRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const lightningCanvas = lightningRef.current;
+    const lightningCtx = lightningCanvas.getContext('2d');
+    const image = new Image();
+    image.src = '/tree.png'; // make sure the image is in your public folder
+    let raindrops = [];
+    let lightningTimeout;
+
+    function createRain() {
+      for (let i = 0; i < 100; i++) {
+        raindrops.push({
+          x: Math.random() * window.innerWidth,
+          y: Math.random() * window.innerHeight,
+          length: Math.random() * 20,
+          opacity: Math.random() * 0.5 + 0.5,
+          speed: Math.random() * 5 + 2
+        });
+      }
+    }
+
+    function drawRain() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+      ctx.strokeStyle = 'rgba(174, 194, 224, 0.5)';
+      ctx.lineWidth = 1;
+      ctx.lineCap = 'round';
+
+      for (let i = 0; i < raindrops.length; i++) {
+        const { x, y, length, opacity, speed } = raindrops[i];
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y + length);
+        ctx.globalAlpha = opacity;
+        ctx.stroke();
+
+        raindrops[i].y += speed;
+        if (raindrops[i].y > window.innerHeight) {
+          raindrops[i].y = -length;
+        }
+      }
+    }
+
+    function drawLightning() {
+        const centerX = window.innerWidth / 2;
+        const centerY = window.innerHeight / 2;
+        const radius = Math.random() * (window.innerWidth / 4) + (window.innerWidth / 4); // randomize the size a bit
+      
+        lightningCtx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+      
+        const gradient = lightningCtx.createRadialGradient(centerX, centerY, 0, centerX, centerY, radius);
+        gradient.addColorStop(0, `rgba(138, 43, 226, ${Math.random() * 0.8 + 0.2})`); // inner color - stronger
+        gradient.addColorStop(1, 'rgba(138, 43, 226, 0)'); // outer color - fully transparent
+      
+        lightningCtx.fillStyle = gradient;
+        lightningCtx.beginPath();
+        lightningCtx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+        lightningCtx.fill();
+      
+        clearTimeout(lightningTimeout);
+        lightningTimeout = setTimeout(() => {
+          lightningCtx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        }, 300 + Math.random() * 200); // hold the 'lightning' a bit before clearing
+      }
+      
+  
+      function animate() {
+        drawRain();
+        requestAnimationFrame(animate);
+      }
+  
+      image.onload = () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        lightningCanvas.width = window.innerWidth;
+        lightningCanvas.height = window.innerHeight;
+        createRain();
+        animate();
+        setInterval(drawLightning, 5000); // adjust for more or less frequent lightning
+      };
+    }, []);
+  
+    return (
+        <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: -1 }}>
+        <canvas ref={lightningRef} style={{ position: 'absolute', width: '100%', height: '100%' }} />
+        <canvas ref={canvasRef} style={{ position: 'absolute', width: '100%', height: '100%', opacity: '0.9' }} />
+      </div>
+    );
+  };
+  
+  export default Background;
---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+// Logo.js
+export default function Logo() {
+    return (
+      <div className="logo-container w-48 mx-auto">
+        <img src="/maidenandmermaid.png" alt="Maiden and Mermaid Logo" className="w-full h-auto" />
+      </div>
+    );
+  }
+  
