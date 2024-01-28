38bffe06dd3228fbf634af2734c3414d01f09326
Robert Greenwood <RobertGreenwood27@gmail.com>
2024-01-28 08:33:15 -0700

    added redbanner

---
+++ src/components/RedBanner.js
@@ -0,0 +1,11 @@
+// RedBanner.js
+
+import React from 'react';
+
+const RedBanner = () => (
+  <div className="bg-red-500 text-white py-2 text-center">
+    <p>Exciting Changes Ahead! Our website is getting a makeover, and we&apos;re thrilled to unveil our fresh look to you soon. Stay tuned for an enchanting experience!</p>
+  </div>
+);
+
+export default RedBanner;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,13 @@
 // _document.tsx
 import { Html, Head, Main, NextScript } from 'next/document';
+import RedBanner from '@/components/RedBanner'
 
 export default function Document() {
   return (
     <Html lang="en" className='dark'>
       <Head />
       <body>
+        <RedBanner/>
         <Main />
         <NextScript />
       </body>
