1ec3b06310e9a1cbe6b4b2a6c6bf2ae77016eb5a
Robert Greenwood <RobertGreenwood27@gmail.com>
2024-01-28 08:20:37 -0700

    updated 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,22 @@
 // pages/404.js
 
 import React from 'react';
-import Link from 'next/link'; // Import Link from next/link
+import Link from 'next/link';
 
 const NotFoundPage = () => (
   <div
     className="min-h-screen bg-cover bg-center flex items-center justify-center"
     style={{
-      backgroundImage: 'url(/working.png)',
+      backgroundImage: 'url(/mystical-background.png)', // Replace with your own background image
     }}
   >
     <div className="bg-black bg-opacity-70 p-6 rounded-lg text-white text-center">
-      <h1 className="text-4xl font-bold mb-2">404 - Page Not Found</h1>
+      <h1 className="text-4xl font-bold mb-2">404 - The Mysterious Void</h1>
       <p className="text-lg mb-4">
-        Oops! It seems like our goofy web developer got a bit lost in the code. This page doesn&apos;t exist yet!
+        Oh no! You&apos;ve stumbled into the uncharted territory of our tarot world. This mystical page is yet to be foreseen by our tarot cards.
       </p>
       <p className="text-lg">
-        But don&apos;t worry, you can always go back to our <Link href="/">home page</Link> and continue your journey with us.
+        Fear not, for your path is clear. Return to the <Link href="/">sanctuary of our home page</Link> and continue your tarot journey.
       </p>
     </div>
   </div>
